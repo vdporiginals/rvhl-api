181f7e4bfaef8d80d67e19fabfd0404dee7ed251
vdp.originals <vdp.originals@gmail.com>
2020-02-22 13:12:04 +0700

    Them cau truc cho project

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+// const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/database');
 
 //load env
 dotenv.config({ path: './config/config.env' });
+//connect DB
+connectDb();
 
+//Route
+
+//App Express
 const app = express();
+//Body parser
+app.use(express.json());
 
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//mount router
+
+//Server listen
 const PORT = process.env.PORT || 5000;
-
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+      .bold
+  )
 );
+
+//handle rejecttions
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
