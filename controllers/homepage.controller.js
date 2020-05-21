8ab5dca4e0dc2a5482ebac6d470305cd754c4c73
vdp.originals <vdp.originals@gmail.com>
2020-05-22 00:20:09 +0700

    fix

--- controllers/homepage.controller.js
+++ controllers/homepage.controller.js
@@ -277,8 +277,7 @@ exports.getAdvertiseBanner = asyncHandler(async (req, res, next) => {
   AdvertiseCategory.find({
     position: 'HomepageAdvertise',
   }).then(async (val) => {
-    if (val !== undefined || val.length != 0) {
-      console.log(val, val[0]._id);
+    if (val[0]._id) {
       let banner = Advertise.find({
         category: val[0]._id,
         page: 'Homepage',
