8a3695b15ebf7a4d9918e0674f02c1d7d790d89e
vdp.originals <vdp.originals@gmail.com>
2020-06-07 21:34:33 +0700

    fix

--- controllers/image-flick.controller.js
+++ controllers/image-flick.controller.js
@@ -14,7 +14,7 @@ const oauth = new Flickr.OAuth(
   process.env.FLICKR_KEY,
   process.env.FLICKR_SECRET
 );
-let signature;
+let secret;
 exports.authFlickr = asyncHandler(async (req, res, next) => {
   // const gallery = req.body;
   oauth
@@ -22,7 +22,8 @@ exports.authFlickr = asyncHandler(async (req, res, next) => {
     .then(function (data) {
       // console.log(data.request.params);
       const token = data.body.oauth_token;
-      const secret = data.body.oauth_token_secret;
+      secret = data.body.oauth_token_secret;
+      console.log(data);
       signature = data.request.params.oauth_signature;
       const url = oauth.authorizeUrl(token, 'write');
       // const verify = 'ba3c906c9ad30537';
@@ -70,33 +71,42 @@ exports.authFlickr = asyncHandler(async (req, res, next) => {
 
 exports.verifyToken = asyncHandler(async (req, res, next) => {
   const { oauthToken, oauthVerifier } = req.body;
-  const tokenSecret = process.env.FLICKR_SECRET;
-  const verifyUrl =
-    `https://www.flickr.com/services/oauth/access_token?oauth_nonce=37026218&oauth_timestamp=1305586309` +
-    `&oauth_verifier=${req.query.oauth_verifier}` +
-    `&oauth_consumer_key=${process.env.FLICKR_KEY}` +
-    `&oauth_signature_method=HMAC-SHA1` +
-    // `&oauth_signature=${data.request.params.oauth_signature}` +
-    `&oauth_version=1.0` +
-    `&oauth_token=${req.query.oauth_token}`;
+  const tokenSecret = secret;
+  oauth
+    .verify(req.query.oauth_token, req.query.oauth_verifier, tokenSecret)
+    .then(function (res) {
+      console.log('oauth token:', res.body.oauth_token);
+      console.log('oauth token secret:', res.body.oauth_token_secret);
+    })
+    .catch(function (err) {
+      console.log('bonk', err);
+    });
+  // const verifyUrl =
+  //   `https://www.flickr.com/services/oauth/access_token?oauth_nonce=37026218&oauth_timestamp=1305586309` +
+  //   `&oauth_verifier=${}` +
+  //   `&oauth_consumer_key=${process.env.FLICKR_KEY}` +
+  //   `&oauth_signature_method=HMAC-SHA1` +
+  //   `&oauth_signature=${data.request.params.oauth_signature}` +
+  //   `&oauth_version=1.0` +
+  //   `&oauth_token=${}`;
   // request.get(verifyUrl);
-  request.get(verifyUrl, async function (err, res, body) {
-    //nếu có lỗi
-    if (err) throw err;
-    // let data = JSON.parse(body);
-    // isValid = await body.data.is_valid;
-    console.log(body);
-    // if (body) {
-    //   return next(
-    //     new ErrorResponse('Not authorized to access this route', 401)
-    //   );
-    // }
+  // request.get(verifyUrl, async function (err, res, body) {
+  //   //nếu có lỗi
+  //   if (err) throw err;
+  //   // let data = JSON.parse(body);
+  //   // isValid = await body.data.is_valid;
+  //   console.log(body);
+  //   // if (body) {
+  //   //   return next(
+  //   //     new ErrorResponse('Not authorized to access this route', 401)
+  //   //   );
+  //   // }
 
-    // return res.status(200).json({
-    //   success: true,
-    //   // data: isValid,
-    // });
-  });
+  //   // return res.status(200).json({
+  //   //   success: true,
+  //   //   // data: isValid,
+  //   // });
+  // });
   // oauth
   //   .verify(oauthToken, oauthVerifier, tokenSecret)
   //   .then(function (res) {
