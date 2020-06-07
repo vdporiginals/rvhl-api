4bea482a821ca62399a3bb359f46f4b754d27a93
vdp.originals <vdp.originals@gmail.com>
2020-06-07 21:54:21 +0700

    update

--- controllers/image-flick.controller.js
+++ controllers/image-flick.controller.js
@@ -18,7 +18,7 @@ let secret;
 exports.authFlickr = asyncHandler(async (req, res, next) => {
   //
   oauth
-    .request('http://api.reviewhalong.vn/api/image/oauth')
+    .request('https://api.reviewhalong.vn/api/image/oauth')
     .then(function (data) {
       // console.log(data.request.params);
       const token = data.body.oauth_token;
